59bb94fb6ae6b1d921765f4082656f5241eb0dbe
Tes Chhayarith <teschhayarith@gmail.com>
2026-07-13 16:16:13 +0700

    fix(tooling): generate Swift bindings in package source

--- pigeons/native_geofence_api.dart
+++ pigeons/native_geofence_api.dart
@@ -6,7 +6,8 @@ import 'package:pigeon/pigeon.dart';
 @ConfigurePigeon(PigeonOptions(
   dartOut: 'lib/src/generated/platform_bindings.g.dart',
   dartPackageName: 'native_geofence',
-  swiftOut: 'ios/Classes/Generated/FlutterBindings.g.swift',
+  swiftOut:
+      'ios/native_geofence/Sources/native_geofence/Generated/FlutterBindings.g.swift',
   kotlinOut:
       'android/src/main/kotlin/com/chunkytofustudios/native_geofence/generated/FlutterBindings.g.kt',
   kotlinOptions:
